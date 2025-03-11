085fa848d7b848002d81681c09739c4a149ca993
dieudonneshaka <158247669+dieudonneshaka@users.noreply.github.com>
2025-03-11 10:40:11 +0200

    Add code witch creates webapi

--- program.cs
+++ program.cs
@@ -1,7 +1,21 @@
+using System;
+using Microsoft.AspNetCore.Builder;
 public class program {
-    
+
     // VSCODE
         public static void main(string[] args) {
+            System.Console.WriteLine("starting");
+
+            // CreateBuilder create a builder (a factor)
+            var bulder = WebApplication.CreateBuilder();
+
+            // Builder. build creates a web app
+
+            var webapi = bulder.build();
+
+
+            
+            // After that we can use  the bulder to create the web service
 
     }
 }
